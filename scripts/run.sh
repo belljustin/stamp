3a98644fa4870a64f62d4d6271cfc072f59faf49
Justin Bell <jbell@circle.com>
2018-11-28 20:31:51 -0500

    Fix run script

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -e
-parent_path=$(dirname ${BASH_SOURCE[0]})/..
-echo $parent_path
+SRCPATH=$(
+  cd $(dirname "$0")/..
+  pwd
+)
+
+echo $SRCPATH
 
 docker-compose -f build/docker-compose.yml up -d db
 rambler -c sql/rambler.json apply -all
@@ -12,21 +16,20 @@ sn=stamp
 # Start the session and window 0 in src folder
 #   This will also be the default cwd for new windows created
 #   via a binding unless overridden with default-path.
-cd $parent_path
-tmux new-session -s "$sn" -n stamp -d
-
-
-# TODO: pass in db creds
-tmux new-window -c "$parent_path" \
+cd $SRCPATH
+tmux new-session -d -s "$sn" \
     -n postgres \
     "psql -h 127.0.0.1 db user"
 
-tmux new-window -c "$parent_path/truffle" \
+tmux new-window -c "$SRCPATH/truffle" \
     -n geth \
-    "$parent_path/scripts/startGeth.sh"
+    "$SRCPATH/truffle/scripts/startGeth.sh; bash"
 
-tmux new-window -c "$parent_path" \
+tmux new-window -c "$SRCPATH" \
     -n stamp \
-    "make go; ./build/stamp -config config.json; bash"
+    "make go; $SRCPATH/build/stamp -config config.json; bash"
+
+tmux set-option -t $sn \
+    remain-on-exit on
 
 tmux attach-session -t "$sn"
